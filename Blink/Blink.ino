27893525f3a468d5b19396444b286f3792bded03
thuhoang-bk <thusupermen@gmail.com>
2022-03-15 19:59:54 +0700

    Initial commit

---
+++ Blink/Blink.ino
@@ -0,0 +1,14 @@
+// LED_BUILTIN = PIN 13
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
---
+++ Test_motor/Test_motor.ino
@@ -0,0 +1,26 @@
+#define enA 8       //OUTPUT PIN - to control motor A
+#define in1 9
+#define in2 10
+
+#define phase_A 2   //INPUT PIN - encoder
+#define phase_B 3
+
+void setup() {
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(phase_A, INPUT);
+  pinMode(phase_B, INPUT);
+
+  digitalWrite(in1, HIGH);  //set rotate direction of motor A
+  digitalWrite(in2, LOW);   
+  analogWrite(enA, 126);    //set speed of motor A
+  
+  Serial.begin(9600);       //start Serial monitor
+
+}
+
+void loop() {
+  Serial.println(digitalRead(phase_A));
+  //see monitor and plotter for output of encoder
+}
